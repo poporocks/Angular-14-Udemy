bd4b50621bcb0fa0b39f21fff759575ec5d18779
Edgar <edgar.lao89@gmail.com>
2022-12-19 17:33:38 -0700

    Ejercicio Tareas finalizado - 20221219

---
+++ Tareas/src/app/components/tareas/tareas.component.ts
@@ -0,0 +1,33 @@
+import { Component } from '@angular/core';
+import { Tarea } from 'src/app/models/Tarea';
+
+@Component({
+  selector: 'app-tareas',
+  templateUrl: './tareas.component.html',
+  styleUrls: ['./tareas.component.css']
+})
+export class TareasComponent {
+  listTareas: Tarea[] = [];
+  nombreTarea = '';
+
+  agregarTarea() {
+    //Crear un objeto tarea
+    const tarea: Tarea = {
+      nombre: this.nombreTarea,
+      estado: false
+    }
+    //Agregar el objeto tarea al array
+    this.listTareas.push(tarea);
+
+    //Reset form
+    this.nombreTarea = '';
+  }
+
+  eliminarTarea(index: number): void {
+    this.listTareas.splice(index, 1);
+  }
+
+  actualizarTarea(tarea: Tarea, index: number): void {
+    this.listTareas[index].estado = !tarea.estado;
+  }
+}
