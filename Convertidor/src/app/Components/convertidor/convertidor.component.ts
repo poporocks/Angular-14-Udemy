8a8be043379a4d548d51f31e2af06e42e861a615
Edgar <edgar.lao89@gmail.com>
2022-12-19 17:50:39 -0700

    Ejercicio convertidor - 20221219

---
+++ Convertidor/src/app/Components/convertidor/convertidor.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-convertidor',
+  templateUrl: './convertidor.component.html',
+  styleUrls: ['./convertidor.component.css']
+})
+export class ConvertidorComponent {
+
+}
