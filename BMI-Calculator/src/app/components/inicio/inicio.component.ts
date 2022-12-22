30a69dfe9ad33310bdcf1aebffcce2a7f9dcbd3f
Edgar <edgar.lao89@gmail.com>
2022-12-21 17:40:06 -0700

    Actualizacion de BMI-Calculator 20221221

--- BMI-Calculator/src/app/components/inicio/inicio.component.ts
+++ BMI-Calculator/src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-inicio',
@@ -9,8 +10,19 @@ export class InicioComponent {
   edad = 33;
   peso = 90;
   altura = 173
+  gender = 'M'
+  
+  constructor(private router: Router) { }
 
   cambiarAltura(event: any) {
     this.altura = event.target.value;
+  }
+
+  seleccionarGenero(genero: string) {
+    this.gender = genero;
+  }
+
+  calcularBMI(){
+    this.router.navigate(['/resultado']);
   }
 }
