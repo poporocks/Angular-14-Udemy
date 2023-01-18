1b54d3e5253c803b0b13f1ab169c3b86d76f8d2e
Edgar <edgar.lao89@gmail.com>
2023-01-17 17:50:04 -0700

    Avances de aplicacion pixabay

--- pixabay/src/app/components/listar-imagen/listar-imagen.component.ts
+++ pixabay/src/app/components/listar-imagen/listar-imagen.component.ts
@@ -10,12 +10,15 @@ import { Subscription } from 'rxjs';
 export class ListarImagenComponent {
   termino: string;
   suscripcion: Subscription;
+  listImagenes: any[] = [];
+  bLoading: boolean = false;
 
   constructor(private _imagenService: ImagenService) { 
     this.termino = '';
     this.suscripcion = this._imagenService.getTerminoBusqueda().subscribe(data => {
       console.log(data);
       this.termino = data;
+      this.bLoading = true;
       this.obtenerImagenes();
     });
   }
@@ -23,12 +26,16 @@ export class ListarImagenComponent {
   obtenerImagenes() {
     this._imagenService.getImagenes(this.termino).subscribe(data => {
       console.log(DataTransfer);
+      this.bLoading = false;
 
       if(data.hits.lenght === 0) {
         this._imagenService.setError('No encontramos ningún resultado.');
         return;
       }
+
+      this.listImagenes = data.hits
     }, error => {
+      this.bLoading = false;
       this._imagenService.setError('Ocurrió un inconveniente al realizar la búsqueda.');
         return;
     });
