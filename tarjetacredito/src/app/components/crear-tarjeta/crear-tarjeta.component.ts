65b8b4f19482b956713399f3e8846b6842e005f2
Edgar <edgar.lao89@gmail.com>
2023-01-19 10:26:17 -0700

    Creacion proyecto tarjetacredito

---
+++ tarjetacredito/src/app/components/crear-tarjeta/crear-tarjeta.component.ts
@@ -0,0 +1,36 @@
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { TarjetaCredito } from '../../models/TarjetaCredito';
+
+@Component({
+  selector: 'app-crear-tarjeta',
+  templateUrl: './crear-tarjeta.component.html',
+  styleUrls: ['./crear-tarjeta.component.css']
+})
+export class CrearTarjetaComponent {
+  form: FormGroup;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      titular: ['', Validators.required],
+      numeroTarjeta: ['', [Validators.required, Validators.minLength(16), Validators.maxLength(16)]],
+      fechaExpiracion: ['', [Validators.required, Validators.minLength(5), Validators.maxLength(5)]],
+      cvv: ['', [Validators.required, Validators.minLength(3), Validators.maxLength(3)]],
+    });
+  }
+
+  crearTarjeta(){
+    console.log(this.form);
+    
+    const TARJETA: TarjetaCredito = {
+      titular: this.form.value.titular,
+      numeroTarjeta: this.form.value.numeroTarjeta,
+      fechaExpiracion: this.form.value.fechaExpiracion,
+      cvv: this.form.value.cvv,
+      fechaCreacion: new Date(),
+      fechaActualizacion: new Date()
+    }
+
+    console.log(TARJETA)
+  }
+}
